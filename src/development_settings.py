36e9a03c282732b40728affe8a69b2d3f8fd3b00
Samuel nganga <samien09@gmail.com>
2018-02-13 15:08:54 +0000

    Update development_settings.py

--- src/development_settings.py
+++ src/development_settings.py
@@ -32,5 +32,5 @@ DATABASES = {
 
 
 DATABASES['default'] =  dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
